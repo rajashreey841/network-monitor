00fc8b16e8593c41bc0f9baf45ae719b635bc9aa
Ravi Kumar Reddy K <rreddyk@cisco.com>
2021-05-09 21:32:27 +0530

    Base templates

--- netmon_project/netmon_project/settings.py
+++ netmon_project/netmon_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'netmon.apps.NetmonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
