b4b164fa6657305708000a35f48c47ae3a8da71d
Ravi Kumar Reddy K <rreddyk@cisco.com>
2021-05-09 22:26:54 +0530

    Optimizing URL Routes

--- netmon_project/netmon/urls.py
+++ netmon_project/netmon/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='netmon-home'),
+    path('', views.home, name='netmon-default'),
     path('home/', views.home, name='netmon-home'),
     path('about/', views.about, name='netmon-about'),
 ]
