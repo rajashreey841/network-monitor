8a2799521d9e04550f66c5f83106f8daca0369d4
Ravi Kumar Reddy K <rreddyk@cisco.com>
2021-05-10 10:34:37 +0530

    Device Model and Migrations

--- netmon_project/netmon/models.py
+++ netmon_project/netmon/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Device(models.Model):
+    dev_name = models.CharField(max_length=100)
+    dev_id = models.IntegerField(max_length=3)
+    dev_type = models.CharField(max_length=100)
+    dev_ip = models.CharField(max_length=16)
+    dev_mac = models.CharField(max_length=18)
+    dev_info = models.CharField(max_length=100)
+    dev_tx_traffic = models.IntegerField(max_length=10)
+    dev_rx_traffic = models.IntegerField(max_length=10)
+    dev_last_updated = models.DateTimeField(default=timezone.now)
+    dev_added_by = models.ForeignKey(User, on_delete=models.CASCADE)
