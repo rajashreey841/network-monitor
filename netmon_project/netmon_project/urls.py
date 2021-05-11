7fea87ae1086a777dde2e0681b40f177792f691e
Ravi Kumar Reddy K <rreddyk@cisco.com>
2021-05-11 10:48:49 +0530

    Register update

--- netmon_project/netmon_project/urls.py
+++ netmon_project/netmon_project/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views as user_views
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('netmon.urls')),
-    path('register/', include('user_views.register'), name='register')
+    path('register/', users_views.register, name='register')
 ]
