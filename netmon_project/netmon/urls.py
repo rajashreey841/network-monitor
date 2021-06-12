f80fc7d1dd630b92efda27d10a8e796a6fb5c048
Ravi Kumar Reddy K <rreddyk@cisco.com>
2021-06-12 19:22:13 +0530

    Add, Update, Delete

--- netmon_project/netmon/urls.py
+++ netmon_project/netmon/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from .views import DeviceListView, DeviceDetailView, DeviceCreateView, DeviceUpdateView, DeviceDeleteView
 
 urlpatterns = [
-    path('home/', views.home, name='netmon-home'),
+    path('home/', DeviceListView.as_view(), name='netmon-home'),
     path('about/', views.about, name='netmon-about'),
+    path('device/<int:pk>/', DeviceDetailView.as_view(), name='device-detail'),
+    path('device/new/', DeviceCreateView.as_view(), name='device-create'),
+    path('device/<int:pk>/new/', DeviceUpdateView.as_view(), name='device-update'),
+    path('device/<int:pk>/new/', DeviceDeleteView.as_view(), name='device-delete'),
 ]
--- netmon_project/netmon/views.py
+++ netmon_project/netmon/views.py
@@ -1,33 +1,35 @@
+from typing import List
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Device
 
-devices = [
-    {
-        'dev_name': 'PROD Server',
-        'dev_id': '1',
-        'dev_type': 'Server',
-        'dev_ip': '192.168.230.245',
-        'dev_mac': '24:EE:9A:F1:D7:AE',
-        'dev_info': 'Windows Service 2012 R2',
-        'dev_tx_traffic': '2343',
-        'dev_rx_traffic': '2343',
-        'dev_last_updated': '2343',
-        'dev_added_by': '2343'
-    },
-    {
-        'dev_name': 'Router',
-        'dev_id': '2',
-        'dev_type': 'Router',
-        'dev_ip': '192.168.230.246',
-        'dev_mac': '24:EE:9A:F1:D7:AE',
-        'dev_info': 'Cisco ISR 4314',
-        'dev_tx_traffic': '3224',
-        'dev_rx_traffic': '3224',
-        'dev_last_updated': '2343',
-        'dev_added_by': '2343'
-    }
-]
+# devices = [
+#     {
+#         'dev_name': 'PROD Server',
+#         'dev_id': '1',
+#         'dev_type': 'Server',
+#         'dev_ip': '192.168.230.245',
+#         'dev_mac': '24:EE:9A:F1:D7:AE',
+#         'dev_info': 'Windows Service 2012 R2',
+#         'dev_tx_traffic': '2343',
+#         'dev_rx_traffic': '2343',
+#         'dev_last_updated': '2343',
+#         'dev_added_by': '2343'
+#     },
+#     {
+#         'dev_name': 'Router',
+#         'dev_id': '2',
+#         'dev_type': 'Router',
+#         'dev_ip': '192.168.230.246',
+#         'dev_mac': '24:EE:9A:F1:D7:AE',
+#         'dev_info': 'Cisco ISR 4314',
+#         'dev_tx_traffic': '3224',
+#         'dev_rx_traffic': '3224',
+#         'dev_last_updated': '2343',
+#         'dev_added_by': '2343'
+#     }
+# ]
 
 # Create your views here.
 def home(request):
@@ -35,6 +37,46 @@ def home(request):
         'devices': Device.objects.all()
     }
     return render(request, 'netmon/home.html', context)
+
+class DeviceListView(ListView):
+    model = Device
+    template_name = 'netmon/home.html'
+    context_object_name = 'devices'
+
+class DeviceDetailView(DetailView):
+    model = Device
+
+class DeviceCreateView(CreateView):
+    model = Device
+    fields = ['dev_name', 'dev_ip']
+
+    def form_valid(self, form):
+        form.instance.dev_added_by = self.request.user
+        return super().form_valid(form)
+
+class DeviceUpdateView(DetailView):
+    model = Device
+    fields = ['dev_name', 'dev_ip']
+
+    def form_valid(self, form):
+        form.instance.dev_added_by = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        device = self.get_object()
+        if self.request.user == device.author:
+            return True
+        return False
+
+class DeviceDeleteView(DetailView):
+    model = Device
+    success_url = '/'
+
+    def test_func(self):
+        device = self.get_object()
+        if self.request.user == device.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'netmon/about.html', {'title': "About"})
